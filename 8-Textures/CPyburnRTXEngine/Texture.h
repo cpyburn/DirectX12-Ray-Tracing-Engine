289eab95dd9a1e23eda64e54208318f6cce3338b
chad pyburn <cpyburn78@yahoo.com>
2026-03-03 09:34:25 -0600

    added dds and wic to texture

--- 8-Textures/CPyburnRTXEngine/Texture.h
+++ 8-Textures/CPyburnRTXEngine/Texture.h
@@ -14,19 +14,21 @@ namespace CPyburnRTXEngine
 	private:
 		static std::mutex m_mutex;
 
+		static std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
 		static std::map<UINT, Microsoft::WRL::ComPtr<ID3D12Resource>> m_texturesUpload;
 		static std::map<UINT, Microsoft::WRL::ComPtr<ID3D12Resource>> m_textures;
 		static std::map<std::string, HeapTexture> m_loadedTextures;
 		static bool TryGetTextureHeap(const std::string& path, Texture::HeapTexture& textureHeap);
 
-
-
 	public:
-		static HeapTexture LoadTextureHeap(const std::shared_ptr<DX::DeviceResources>& deviceResources, const std::string& path, ID3D12GraphicsCommandList* commandList);
+		static void CreateDeviceDependentResources(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		static HeapTexture LoadTextureHeap(const std::string& path, ID3D12GraphicsCommandList* commandList);
 		static void Release();
 		static void ReleaseUploadByHeapPosition(UINT heapPosition);
-		static Texture::HeapTexture LoadCustomTexture(const std::shared_ptr<DX::DeviceResources>& deviceResources, ID3D12GraphicsCommandList* commandList, const std::wstring& wFileName);
-		static Texture::HeapTexture LoadCustomTexture(const std::shared_ptr<DX::DeviceResources>& deviceResources, ID3D12GraphicsCommandList* commandList, const DXGI_FORMAT& format, const UINT& width, const UINT& height, const uint8_t* data, const size_t& rowPitch, const std::wstring& wFileName);
+		static Texture::HeapTexture LoadCustomDDSTexture(ID3D12GraphicsCommandList* commandList, const std::wstring& wFileName);
+		static Texture::HeapTexture LoadCustomWICTexture(ID3D12GraphicsCommandList* commandList, const std::wstring& wFileName);
+		static Texture::HeapTexture LoadCustomTexture(ID3D12GraphicsCommandList* commandList, const DXGI_FORMAT& format, const UINT& width, const UINT& height, const uint8_t* data, const size_t& rowPitch, const std::wstring& wFileName);
 		static void RemoveHeapPosition(UINT position);
 		~Texture();
 	};

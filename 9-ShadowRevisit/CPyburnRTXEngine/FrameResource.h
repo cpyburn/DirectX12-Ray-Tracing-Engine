8a0d0b329f1372504a965e0ea335404cd5961ca0
cpyburn <cpyburn78@yahoo.com>
2026-03-04 08:42:25 -0600

    starting revisit to shadow rays

---
+++ 9-ShadowRevisit/CPyburnRTXEngine/FrameResource.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace CPyburnRTXEngine
+{
+	class FrameResource
+	{
+	public:
+		static const int COMMAND_LIST_COUNT = 2;
+
+		static const int COMMAND_LIST_SCENE_0 = 0;
+		static const int COMMAND_LIST_POST_1 = 1;
+
+		ID3D12CommandList* BatchSubmit[COMMAND_LIST_COUNT] = {};
+	private:
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocators[COMMAND_LIST_COUNT];
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList4> m_commandLists[COMMAND_LIST_COUNT];
+	public:
+		FrameResource();
+		~FrameResource();
+
+		void Init(DX::DeviceResources* deviceResources);
+		ID3D12GraphicsCommandList4* ResetCommandList(const int commandList, ID3D12PipelineState* pInitialState = nullptr);
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList4> GetCommandList(int commandList) { return m_commandLists[commandList]; }
+		void Release();
+	};
+}
---
+++ 9-ShadowRevisit/CPyburnRTXEngine/GraphicsContexts.cpp
@@ -0,0 +1,128 @@
+#include "pchlib.h"
+#include "GraphicsContexts.h"
+
+using namespace CPyburnRTXEngine;
+
+UINT GraphicsContexts::c_descriptorSize;
+Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> GraphicsContexts::c_heap;
+
+UINT GraphicsContexts::m_heapPositionCounter;
+std::vector<UINT> GraphicsContexts::m_availableHeapPositions;
+std::map<UINT, UINT> GraphicsContexts::m_multiUseHeapPositions;
+std::mutex GraphicsContexts::m_mutexMultiUseHeapPositions;
+
+namespace CPyburnRTXEngine
+{
+	GraphicsContexts::GraphicsContexts()
+	{
+
+	}
+
+	GraphicsContexts::~GraphicsContexts()
+	{
+		Release();
+	}
+
+	D3D12_CPU_DESCRIPTOR_HANDLE GraphicsContexts::GetCpuHandle(const UINT& index)
+	{
+		//CD3DX12_CPU_DESCRIPTOR_HANDLE(GraphicsContexts::c_heap->GetCPUDescriptorHandleForHeapStart(), index, GraphicsContexts::c_descriptorSize);
+		D3D12_CPU_DESCRIPTOR_HANDLE handle = c_heap->GetCPUDescriptorHandleForHeapStart();
+		handle.ptr += index * c_descriptorSize;
+		return handle;
+	}
+
+	D3D12_GPU_DESCRIPTOR_HANDLE GraphicsContexts::GetGpuHandle(const UINT& index)
+	{
+		//CD3DX12_GPU_DESCRIPTOR_HANDLE(GraphicsContexts::c_heap->GetGPUDescriptorHandleForHeapStart(), index, GraphicsContexts::c_descriptorSize);
+		D3D12_GPU_DESCRIPTOR_HANDLE handle = c_heap->GetGPUDescriptorHandleForHeapStart();
+		handle.ptr += index * c_descriptorSize;
+		return handle;
+	}
+
+	void GraphicsContexts::AddMultiHeapPosition(UINT heapPosition)
+	{
+		m_mutexMultiUseHeapPositions.lock();
+		auto iter = m_multiUseHeapPositions.find(heapPosition);
+		if (iter != m_multiUseHeapPositions.end())
+		{
+			iter->second++;
+		}
+		else
+		{
+			m_multiUseHeapPositions.insert(std::pair<UINT, UINT>(heapPosition, 1));
+		}
+		m_mutexMultiUseHeapPositions.unlock();
+	}
+
+	bool GraphicsContexts::RemoveHeapPosition(UINT heapPosition)
+	{
+		bool didErase = false;
+
+		m_mutexMultiUseHeapPositions.lock();
+		auto iter = m_multiUseHeapPositions.find(heapPosition);
+		if (iter != m_multiUseHeapPositions.end())
+		{
+			// remove one from multiheap
+			iter->second--;
+			if (iter->second == 0)
+			{
+				// remove from multiheap if all are gone
+				m_multiUseHeapPositions.erase(iter);
+				DebugTrace("Heap position reclaimed: %s\n", (std::to_string(heapPosition)).c_str());
+				m_availableHeapPositions.push_back(heapPosition);
+				didErase = true;
+			}
+		}
+		else
+			m_availableHeapPositions.push_back(heapPosition); // if it wasn't a multiuse then reuse
+
+		m_mutexMultiUseHeapPositions.unlock();
+
+		return didErase;
+	}
+
+	UINT GraphicsContexts::GetAvailableHeapPosition()
+	{
+		m_mutexMultiUseHeapPositions.lock();
+		// NOTE: was having issues tying to claim previously used heap so commented out for now, just incriments until it runs out for now but added a catch
+		assert(m_heapPositionCounter != UINT32_MAX);
+
+		UINT value = m_heapPositionCounter;
+		if (m_availableHeapPositions.size() > 0)
+		{
+			value = m_availableHeapPositions[0];
+			DebugTrace("Heap position reused: %s\n", (std::to_string(value)).c_str());
+			m_availableHeapPositions.erase(m_availableHeapPositions.begin());
+		}
+		else
+		{
+			m_heapPositionCounter++;
+			DebugTrace("Heap position used: %s\n", (std::to_string(value)).c_str());
+		}
+
+		m_mutexMultiUseHeapPositions.unlock();
+
+		return value;
+	}
+
+	void GraphicsContexts::CreateDeviceDependentResources(ID3D12Device* d3dDevice)
+	{
+		c_descriptorSize = d3dDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+		D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+		heapDesc.NumDescriptors = DX::DeviceResources::c_backBufferCount // Vertex constant buffers per frame 
+			+ 100; // Arbitrary large number for now.
+		heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+		// This flag indicates that this descriptor heap can be bound to the pipeline and that descriptors contained in it can be referenced by a root table.
+		heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+
+		DX::ThrowIfFailed(d3dDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&c_heap)));
+		c_heap->SetName(L"Descriptor Heap from GraphicsContexts");
+	}
+
+	void GraphicsContexts::Release()
+	{
+		c_heap.Reset();
+	}
+}
+
